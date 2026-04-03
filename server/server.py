a8cc025907782c3e15bda3abfbb76fb7f8b390b5
Alfin19122004 <alfinblesson19@gmail.com>
2026-04-03 15:28:23 +0530

    Ready for Render hosting

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -237,4 +238,6 @@ def lock():
         RESULT_LOCK = True
         return jsonify({"status": "locked"})
     return jsonify({"status": "wrong_password"})
-app.run(host='0.0.0.0', port=5000)
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
